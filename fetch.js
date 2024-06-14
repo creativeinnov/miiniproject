7143fb051929d17adbfe34f303bf258899c0b148
creativeinnov <mehtalaxmi1234@gmail.com>
2024-06-14 16:54:56 +0530

    new files

---
+++ fetch.js
@@ -0,0 +1,22 @@
+//url = "https://reqres.in/api/users?page=2"
+function fetchData(pageno){
+  url = `https://reqres.in/api/users?page=${pageno}`
+  fetch(url)
+  .then(res => res.json())
+  .then(result =>{
+    output = ''
+    for(let i=0; i<result['data'].length; i++){
+      user = result['data'][i]
+      output += `<tr>
+        <td>${user.id}</td>
+        <td>${user.email}</td>
+        <td>${user.first_name}</td>
+        <td>${user.last_name}</td>
+        <td><td> <img src = "${user.avatar}" alt =""></img></td></td>
+      </tr>`
+    }
+    console.table(result['data'])
+    document.getElementById('records').innerHTML = output
+    //console.log(result)
+  })
+  }
